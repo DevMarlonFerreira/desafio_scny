eb7f94bb0a4bbe607da621903dd07fec87c40c65
Marlon <devmarlonferreira@gmail.com>
2023-06-28 10:56:20 -0300

    Update

--- src/components/cliente/Table.tsx
+++ src/components/cliente/Table.tsx
@@ -8,8 +8,10 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { TextField } from "@mui/material";
 import { ICliente } from "typings/ICliente.d";
 import { IconButton } from "@mui/material";
+import Button from "@mui/material/Button";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ClienteDataService from "app/services/cliente.service";
 const ModalEditar = lazy(() => import("./ModalEditar"));
@@ -18,10 +20,10 @@ const ModalDelete = lazy(() => import("./ModalDelete"));
 export default function BasicTable() {
   const [showPut, setShowPut] = useState(false);
   const [showDel, setShowDel] = useState(false);
-
   const [rows, setRows] = useState<ICliente[]>();
-
   const [cliente, setCliente] = useState<ICliente>();
+
+  const [filter, setFilter] = useState<number>(0);
 
   const handlePut = useCallback(() => setShowPut(!showPut), [showPut]);
 
@@ -34,15 +36,38 @@ export default function BasicTable() {
     return data;
   };
 
-  useEffect(() => {
-    getData()
-    .then(data => {
+  const handleDataFilter = useCallback(async () => {
+    const { data } = await ClienteDataService.get(filter);
+    setRows([data]);
+  }, [filter]);
+
+  const handleData = useCallback(async () => {
+    getData().then((data) => {
       setRows(data);
-    })
+    });
+  }, []);
+
+  useEffect(() => {
+    getData().then((data) => {
+      setRows(data);
+    });
   }, [handleDel, handlePut]);
 
   return (
     <TableContainer component={Paper}>
+      <TextField
+        id="standard-basic"
+        label="Pesquisar cliente"
+        variant="standard"
+        onChange={(e) => setFilter(parseInt(e.target.value))}
+      />
+
+      <Button variant="contained" onClick={(e) => handleDataFilter()}>
+        Pesquisar
+      </Button>
+      <Button variant="contained" onClick={(e) => handleData()}>
+        Exibir todos
+      </Button>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
@@ -65,7 +90,8 @@ export default function BasicTable() {
               <TableCell component="th" scope="row">
                 {row.nome}
               </TableCell>
-              <TableCell align="center">{row.tipoDocumento}</TableCell>
+              {/* <TableCell align="center">{row.tipoDocumento}</TableCell> */}
+              <TableCell align="center">{row.id}</TableCell>
               <TableCell align="center">{row.numeroDocumento}</TableCell>
               <TableCell align="center">{row.uf}</TableCell>
               <TableCell align="center">{row.cidade}</TableCell>
